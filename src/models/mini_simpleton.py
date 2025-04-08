2545eba54666987f2738f320a9cffb7ae434ef91
GrygoryZach <grishazschepilo@gmail.com>
2025-04-08 22:51:25 +0200

    feat: sample model with convolutions

--- src/models/mini_simpleton.py
+++ src/models/mini_simpleton.py
@@ -2,23 +2,39 @@ from torch import nn
 
 
 class MiniSimpleton(nn.Module):
-    def __init__(self):
+    def __init__(self, device):
         super(MiniSimpleton, self).__init__()
+        self.relu = nn.ReLU()
 
-        self.flatten = nn.Flatten(start_dim=-3)
-        self.fc1 = nn.Linear(28 * 28, 32)
-        self.relu1 = nn.ReLU()
-        self.fc2 = nn.Linear(32, 32)
-        self.relu2 = nn.ReLU()
-        self.fc3 = nn.Linear(32, 10)
-        self.sigmoid = nn.Sigmoid()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.maxpool1 = nn.MaxPool2d(2, 2)
+
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.maxpool2 = nn.MaxPool2d(2, 2)
+
+        self.flatten = nn.Flatten()
+
+        self.fc1 = nn.Linear(64 * 7 * 7, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 10)
+        self.to(device)
 
     def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.maxpool1(x)
+
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.maxpool2(x)
+
         x = self.flatten(x)
+
         x = self.fc1(x)
-        x = self.relu1(x)
+        x = self.relu(x)
+
         x = self.fc2(x)
-        x = self.relu2(x)
+        x = self.relu(x)
+
         x = self.fc3(x)
-        x = self.sigmoid(x)
         return x
